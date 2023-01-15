a17ec2a30b64b2b47f69c4026bd491ff41f55ebb
Dev_tian <c.cleofas@psp.team>
2023-01-15 13:56:45 +0800

    No Added file, Create changes only

--- authentication/views.py
+++ authentication/views.py
@@ -111,7 +111,7 @@ def signin(request):
         
         if user is not None:
             login(request, user)
-            fname = user.first_name
+            # fname = user.first_name
             messages.success(request, "Logged In Sucessfully!!")
             # return render(request, 'authentication/dashboard.html')
             # return render(request, "authentication/dashboard.html",{"fname":fname})
