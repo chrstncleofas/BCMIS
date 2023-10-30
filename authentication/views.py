2a3144da36f173fa8c7751c238786cd6df889bf0
Christian Cleofas <c.cleofas@psp.team>
2023-10-30 18:30:50 +0800

    NO_TICKET - Test features

--- authentication/views.py
+++ authentication/views.py
@@ -26,27 +26,27 @@ def signup(request):
         email = request.POST.get('email')
         pass1 = request.POST.get('pass1')
         pass2 = request.POST.get('pass2')
-        
+
         # if User.objects.filter(username=username):
         #     messages.error(request, "Username already exist! Please try some other username.")
         #     return redirect('home')
-        
+
         # if User.objects.filter(email=email).exists():
         #     messages.error(request, "Email Already Registered!!")
         #     return redirect('home')
-        
+
         # if len(username)>20:
         #     messages.error(request, "Username must be under 20 charcters!!")
         #     return redirect('home')
-        
+
         # if pass1 != pass2:
         #     messages.error(request, "Passwords didn't matched!!")
         #     return redirect('home')
-        
+
         # if not username.isalnum():
         #     messages.error(request, "Username must be Alpha-Numeric!!")
         #     return redirect('home')
-        
+
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
@@ -54,19 +54,19 @@ def signup(request):
         # myuser.is_active = False
         myuser.save()
         messages.success(request, "Your Account has been created succesfully!! Please check your email to confirm your email address in order to activate your account.")
-        
+
         # Welcome Email
         # subject = "Welcome to GFG- Django Login!!"
-        # message = "Hello " + myuser.first_name + "!! \n" + "Welcome to GFG!! \nThank you for visiting our website\n. We have also sent you a confirmation email, please confirm your email address. \n\nThanking You\nAnubhav Madhav"        
+        # message = "Hello " + myuser.first_name + "!! \n" + "Welcome to GFG!! \nThank you for visiting our website\n. We have also sent you a confirmation email, please confirm your email address. \n\nThanking You\nAnubhav Madhav"
         # from_email = settings.EMAIL_HOST_USER
         # to_list = [myuser.email]
         # send_mail(subject, message, from_email, to_list, fail_silently=True)
-        
+
         # Email Address Confirmation Email
         # current_site = get_current_site(request)
         # email_subject = "Confirm your Email @ GFG - Django Login!!"
         # message2 = render_to_string('email_confirmation.html',{
-            
+
         #     'name': myuser.first_name,
         #     'domain': current_site.domain,
         #     'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
@@ -80,10 +80,10 @@ def signup(request):
         # )
         # email.fail_silently = True
         # email.send()
-        
+
         return redirect('signin')
-        
-        
+
+
     return render(request, 'authentication/signup.html')
 
 
@@ -109,9 +109,9 @@ def signin(request):
     if request.method == 'POST':
         username = request.POST['username']
         pass1 = request.POST['pass1']
-        
+
         user = authenticate(username=username, password=pass1)
-        
+
         if user is not None:
             login(request, user)
             # fname = user.first_name
@@ -122,12 +122,11 @@ def signin(request):
         else:
             messages.error(request, "Bad Credentials!!")
             return redirect('home')
-    
+
     return render(request, 'authentication/signin.html')
 
-def dashboard(request): 
+def dashboard(request):
     return render(request, 'authentication/dashboard.html')
-
 
 def signout(request):
     logout(request)
